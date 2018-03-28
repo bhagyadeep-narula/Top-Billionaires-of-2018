bada7784f5277b77a5f97519b002df6ee8cf5079
Jesse <Jesseokeya@gmail.com>
2018-03-28 12:14:47 -0400

    bug fixes

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = "8000"
+const PORT = "8000";
+
+app.use(express.static(path.join(__dirname + "/src/resources")));
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/src/index.html")
 })
-
-app.use(express.static(path.join(__dirname)));
-
 
 app.listen(PORT, function(){
   console.log("Server is running on port");
