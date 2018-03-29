f4957b569b0abe6331246d05f856a2a4003fb95d
Bhagyadeep <anuraggagneja8@gmail.com>
2018-03-28 23:35:49 -0400

    Done

--- src/resources/js/App.js
+++ src/resources/js/App.js
@@ -1,11 +1,11 @@
-const url = "https://forbes400.herokuapp.com/api/forbes400/real-time";
+var url = "https://forbes400.herokuapp.com/api/forbes400/real-time";
 
 $.get(url, function(response, status){
-  console.log(response)
-  for (var i = 0; i < 100; i++) {
-    $(".narula").append(beauty(response[i]))
+  console.log(response);
+  for (var i = 0; i < 400; i++) {
+    $(".narula").append(beauty(response[i]));
   }
-})
+});
 
 
 
@@ -15,10 +15,10 @@ function beauty(data) {
     <img class="card-img-top" src=${data.squareImage} alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">${data.name}</h5>
-      <h5 class="card-text">${data.realTimeWorth}</h5>
+      <h5 class="card-text">${(Math.floor(data.realTimeWorth)/ 1000).toFixed(2)} Billions </h5>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
 </div>
-<br/>`
+<br/>`;
 }
